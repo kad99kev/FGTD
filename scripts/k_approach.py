8c0d0781419038a099837d0af6c4d6d427d1c79d
Kevlyn Kadamala <43489154+kad99kev@users.noreply.github.com>
2020-06-20 22:34:36 +0530

    Appearance done

--- scripts/k_approach.py
+++ scripts/k_approach.py
@@ -1,6 +1,6 @@
 import random
 
-import pandas as pd
+# import pandas as pd
 import numpy as np
 
 # df = pd.read_csv('./dataset/list_attr_celeba.csv')
@@ -82,10 +82,7 @@ def generate_face_structure(face_attributes, gender):
 def generate_facial_hair(facial_hair_attributes, gender):
 	build = ['has a', 'wears a', 'sports a', 'grows a']
 
-	if gender:
-		sentence = 'He'
-	else:
-		sentence = 'She'
+	sentence = 'He' if gender else 'She'
 	
 	if len(facial_hair_attributes) == 1:
 		attribute = ' '.join(facial_hair_attributes[0].lower().split('_')) if facial_hair_attributes[0] != '5_o_Clock_Shadow' else '5 o\' clock shadow'
@@ -108,15 +105,11 @@ def generate_facial_hair(facial_hair_attributes, gender):
 # Hairstyle 
 def generate_hairstyle(hairstyle_attributes, gender):
 	hair_type = {'Bald', 'Straight_Hair', 'Wavy_Hair', 'Receding_Hairline'}
-	hair_colour = {'Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Gray_Hair'}
 
 	def return_formation(gender, style=None, colour=None):
 		
 		if random.random() <= 0.5:
-			if gender:
-				sentence = 'His'
-			else:
-				sentence = 'Her'
+			sentence = 'His' if gender else 'Her'
 
 			if style is not None and colour is not None:
 				if random.random() <= 0.5:
@@ -129,10 +122,7 @@ def generate_hairstyle(hairstyle_attributes, gender):
 			else:
 				return sentence + ' hair is ' + colour + ' in colour.'
 		else:
-			if gender:
-				sentence = 'He'
-			else:
-				sentence = 'She'
+			sentence = 'He' if gender else 'She'
 			
 			if style is not None and colour is not None:
 				if random.random() <= 0.5:
@@ -144,8 +134,6 @@ def generate_hairstyle(hairstyle_attributes, gender):
 				return sentence + ' has ' + style + ' hair.'
 			else:
 				return sentence + ' hair ' + colour + ' hair.'
-
-
 
 	if len(hairstyle_attributes) == 1:
 	
@@ -173,23 +161,64 @@ def generate_hairstyle(hairstyle_attributes, gender):
 			colour = hairstyle_attributes[0].lower().split('_')[0]
 			return return_formation(gender, style, colour)
 	
-#faical features
-def face_features(face_feature, gender):
-	if gender:
-		sentence = 'He'
-	else:
-		sentence = 'She'
+# Appearance
+def generate_appearance(face_feature, gender):
 
-	if len(face_feature) == 1:
-		return (sentence + ' ' + facial_features[face_feature[0]])
-	else:
-		for i in range(len(face_feature)):
-			if i < len(face_feature) - 1:
-				sentence = sentence + ' ' + facial_features[face_feature[i]] + ', '
+	is_smiling = 'Smiling' in face_feature
+	smile_begin = False if not is_smiling else True if random.random() <= 0.5 else False
+	qualities = list(set(face_feature) & {'Young', 'Attractive'})
+	extras = list(set(face_feature) & {'Pale_Skin', 'Heavy_Makeup', 'Rosy_Cheeks'})
+
+	sentence = random.choice(['He', 'The man', 'The gentleman', 'The male']) if gender else random.choice(['She', 'The woman', 'The lady', 'The female'])
+	
+	if is_smiling and smile_begin:
+		sentence += ' is smiling'
+		sentence += ' and' if len(qualities) == 0 else ',' if len(qualities) > 0 else ''
+
+	if len(qualities) == 1:
+		sentence += random.choice([' looks', ' is', ' seems']) + ' ' + qualities[0].lower()
+		sentence += ' and' if len(extras) == 1 and smile_begin else ',' if len(extras) > 0 else ''
+	elif len(qualities) > 1:
+		sentence += random.choice([' looks', ' is', ' seems'])
+		for i in range(len(qualities)):
+			attribute = qualities[i].lower()
+
+			if i == len(qualities) - 1 and len(extras) == 0:
+				sentence = sentence[:-1]
+				sentence += ' and ' + attribute
 			else:
-				sentence = sentence[:-2]
-				sentence = sentence + ' and ' + facial_features[face_feature[-1]] + '.'
-		return sentence
+				sentence += ' ' +  attribute + ','
+
+	if is_smiling and not smile_begin:
+		if len(extras) == 0:
+			sentence = sentence.replace(' and', ',')
+			sentence += ' and'
+		sentence += ' is smiling'
+		sentence += ',' if len(extras) > 1 else ' and' if len(extras) == 1 else ''
+
+
+	extras = [' '.join(e.split('_')) for e in extras]
+
+	if len(extras) == 0:
+		return sentence + '.'
+	elif len(extras) == 1:
+		return sentence + ' has ' + extras[0].lower() + '.'
+	else:
+		sentence += ' has'
+		for i in range(len(extras)):
+			attribute = extras[i].lower()
+			
+			if i == len(extras) - 1:
+				sentence = sentence[:-1]
+				sentence += ' and ' + attribute
+			else:
+				sentence += ' ' + attribute + ','
+
+		return sentence + '.'
+
+
+
+#################### Test ####################
 
 # test_features = [['High_Cheekbones', 'Oval_Face'], ['Chubby'], ['Chubby', 'Double_Chin'], ['High_Cheekbones', 'Chubby'], ['High_Cheekbones', 'Chubby', 'Oval_Face']]
 # for f in test_features:
@@ -199,9 +228,16 @@ def face_features(face_feature, gender):
 # for f in test_features:
 # 	print(generate_facial_hair(f, True))
 
-test_features = [['Straight_Hair', 'Brown_Hair'], ['Bald'], ['Receding_Hairline', 'Brown_Hair'], ['Wavy_Hair', 'Gray_Hair'], ['Straight_Hair', 'Blond_Hair']]
+# test_features = [['Straight_Hair', 'Brown_Hair'], ['Bald'], ['Receding_Hairline', 'Brown_Hair'], ['Wavy_Hair', 'Gray_Hair'], ['Straight_Hair', 'Blond_Hair']]
+# for f in test_features:
+# 	print(generate_hairstyle(f, True))
+
+test_features = [['Attractive', 'Young', 'Pale_Skin', 'Smiling', 'Rosy_Cheeks'], ['Smiling', 'Rosy_Cheeks'], ['Attractive', 'Smiling', 'Rosy_Cheeks', 'Heavy_Makeup'], ['Attractive', 'Smiling', 'Heavy_Makeup'], ['Young', 'Smiling', 'Heavy_Makeup'], ['Attractive', 'Smiling', 'Young'], ['Attractive', 'Young']]
 for f in test_features:
-	print(generate_hairstyle(f, True))
+	print(generate_appearance(f, True))
+
+
+#################### Working ####################
 
 # for i in df.index:
 	
