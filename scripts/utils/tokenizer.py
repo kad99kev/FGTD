5123218deda0311fe025587ae5ed819c35c3f4bb
Kevlyn Kadamala <43489154+kad99kev@users.noreply.github.com>
2020-07-15 11:28:45 +0530

    Changed to utils

--- scripts/utils/tokenizer.py
+++ scripts/utils/tokenizer.py
@@ -9,7 +9,7 @@ np.random.seed(0)
 
 class Tokenizer:
     '''
-    DataLoader class to load dictionary and perform tokenization tasks
+    Tokenizer class to load dictionary and perform tokenization tasks
     '''
 
     def __init__(self, saved_location, csv_source=None, df=None, word_dict=None):
@@ -32,8 +32,9 @@ class Tokenizer:
         self.word_dict = word_dict
         print("Creating reverse dictionary")
         self.reverse_map = list(self.word_dict.items())
+        self.use_cuda = torch.cuda.is_available()
 
-    def convert_sentence_to_indices(self, sentence, return_with_cuda=True):
+    def convert_sentence_to_indices(self, sentence, return_as_tensor=True):
         '''
         Converts a sentence to its indices
         '''
@@ -48,9 +49,11 @@ class Tokenizer:
         length = len(indices)
         indices += [self.EOS] * (self.MAX_LEN - len(indices))
 
-        if return_with_cuda:
+        if return_as_tensor:
             indices = np.array(indices)
-            indices = torch.tensor(indices).cuda()
+            indices = torch.tensor(indices)
+            if self.use_cuda:
+                indices = indices.cuda()
 
         return indices, length
 
@@ -67,9 +70,55 @@ class Tokenizer:
             lengths_batch.append(length)
 
         indices_batch = np.array(indices_batch)
-        indices_batch = torch.tensor(indices_batch).cuda()
+        indices_batch = torch.tensor(indices_batch)
+        if self.use_cuda:
+            indices_batch = indices_batch.cuda()
 
         return indices_batch, lengths_batch
+
+    def convert_indices_to_sentence(self, indices, length=None):
+        '''
+        Converts an array of indices to its respective sentence
+        '''
+
+        if length is not None: indices = indices[:length]
+
+        def convert_index_to_word(idx):
+
+            idx = idx.data
+            if idx == 0:
+                return "EOS"
+            elif idx == 1:
+                return "UNK"
+            
+            search_idx = idx - 2
+            if search_idx >= len(self.reverse_map):
+                return "NA"
+            
+            word, idx_ = self.reverse_map[search_idx]
+
+            assert idx_ == idx
+            return word
+        
+        words = [convert_index_to_word(idx) for idx in indices]
+
+        return ' '.join(words)
+    
+    def convert_batch_indices_to_sentences(self, batch_indices, lengths=None):
+        '''
+        Converts a batch of indices to sentences
+        '''
+
+        sentences = []
+        for i, indices in enumerate(batch_indices):
+            if lengths is not None:
+                length = lengths[i]
+                sentence = self.convert_indices_to_sentence(indices, length)
+            else:
+                sentence = self.convert_indices_to_sentence(indices)
+            sentences.append(sentence)
+
+        return sentences
 
     def __preprocess_sentence(self, sentence):
         '''
